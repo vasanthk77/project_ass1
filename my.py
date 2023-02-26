dc63ef608704b5b8569bba57c10d4ff72d7bf0c6
Vasanth K <116260385+vasanthk77@users.noreply.github.com>
2023-02-26 23:24:03 +0530

    Update my.py

--- my.py
+++ my.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from matplotlib import image
-image=image.imread(project_ass1/resources/vasanth.jpeg")
-st.image(image,width=200)
+#image=image.imread(project_ass1/resources/vasanth.jpeg")
+#st.image(image,width=200)
 st.header(":blue[VASANTH K]")
 st.subheader('Data science intern at:red[Innomatics Research Labs]:sunglasses:')
 st.subheader("Student at :green[presidency university:blue[(computer science)]]")
